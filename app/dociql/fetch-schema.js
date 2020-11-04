603a68ac2221d271cc2449186b5e1beaad0d99cb
Lars Bonnes <lars.bonnes.ext@bestsecret.com>
2020-11-04 08:49:51 +0100

    Add operation name "IntrospectionQuery" to the request
    Although being optional, it can be a problem to not have the operationName defined in some server implementations. So it does not hurt to include it by default.

--- app/dociql/fetch-schema.js
+++ app/dociql/fetch-schema.js
@@ -6,6 +6,7 @@ const converter = require('graphql-2-json-schema');
 module.exports = function (graphUrl) {
 
     const requestBody = {
+        operationName: "IntrospectionQuery",
         query: graphql.introspectionQuery
     };
 
