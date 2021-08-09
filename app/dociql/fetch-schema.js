57540fc0a8e9ae6a0c83708cbeed218fb9593192
Dave Sullivan <dave@davesullivan.ca>
2021-08-09 11:12:07 -0400

    added support for generating docs from a GraphQL IDL schema file

--- app/dociql/fetch-schema.js
+++ app/dociql/fetch-schema.js
@@ -1,9 +1,36 @@
-const { getIntrospectionQuery, buildClientSchema } = require('graphql')
+const { graphqlSync, buildSchema, getIntrospectionQuery, buildClientSchema } = require('graphql')
 const request = require("sync-request")
+const fs = require('fs');
 
 const converter = require('graphql-2-json-schema');
 
 module.exports = function (graphUrl, authHeader) { 
+    if (graphUrl.includes("file://")) {
+        return fetchSchemaFromFile(graphUrl);
+    } else {
+        return fetchSchemaFromUrl(graphUrl, authHeader);
+    }
+    
+
+}
+
+function fetchSchemaFromFile(graphUrl) {
+  const filePath = graphUrl.replace("file://", "");
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+
+  const graphQLSchema = buildSchema(fileContent);
+  
+  const introspection = graphqlSync(graphQLSchema, getIntrospectionQuery()).data;
+
+  const jsonSchema = converter.fromIntrospectionQuery(introspection);
+
+  return {
+    jsonSchema,
+    graphQLSchema
+  }
+}
+
+function fetchSchemaFromUrl(graphUrl, authHeader) {
     const requestBody = {
         operationName: "IntrospectionQuery",
         query: getIntrospectionQuery()
