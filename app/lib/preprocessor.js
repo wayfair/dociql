5b13079ce28d464474f1ac19239efe7650fc5f74
spnve <nat@page365.net>
2021-09-05 13:52:13 +0700

    document version 1

--- app/lib/preprocessor.js
+++ app/lib/preprocessor.js
@@ -6,7 +6,7 @@ var httpMethods = ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', '
 // Preprocessor for the Swagger JSON so that some of the logic can be taken
 // out of the template.
 
-module.exports = function(options, specData) {
+module.exports = function (options, specData) {
   if (!options.specFile) {
     console.warn("[WARNING] preprocessor must be given 'options.specFile'.  Defaulting to 'cwd'.")
     options.specFile = process.cwd()
@@ -27,10 +27,10 @@ module.exports = function(options, specData) {
   // The "body"-parameter in each operation is stored in a
   // separate field "_request_body".
   if (copy.paths) {
-    Object.keys(copy.paths).forEach(function(pathName) {
+    Object.keys(copy.paths).forEach(function (pathName) {
       var path = copy.paths[pathName]
       var pathParameters = path.parameters || []
-      Object.keys(path).forEach(function(method) {
+      Object.keys(path).forEach(function (method) {
         if (httpMethods.indexOf(method) < 0) {
           delete path[method]
           return
@@ -40,7 +40,7 @@ module.exports = function(options, specData) {
         operation.method = method
         // Draw links from tags to operations referencing them
         var operationTags = operation.tags || ['default']
-        operationTags.forEach(function(tag) {
+        operationTags.forEach(function (tag) {
           if (!tagsByName[tag]) {
             // New implicit declaration of tag not defined in global "tags"-object
             // https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md#user-content-swaggerTags
@@ -59,7 +59,7 @@ module.exports = function(options, specData) {
         // Join parameters with path-parameters
         operation.parameters = (operation.parameters || [])
           .concat(pathParameters)
-          .filter(function(param) {
+          .filter(function (param) {
             if (param.in === 'body') {
               operation._request_body = param
               return false
